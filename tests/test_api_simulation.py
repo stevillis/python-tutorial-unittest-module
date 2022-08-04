68b460d00c3234cd5b44d405558238d62adc8049
Stévillis Sousa <stevillis@hotmail.com>
2022-08-04 16:43:17 -0400

    feat: mock exception

--- tests/test_api_simulation.py
+++ tests/test_api_simulation.py
@@ -2,22 +2,25 @@
 
 from unittest import TestCase, mock
 
-from core.api_simulation import google
-
 
 class TestAPI(TestCase):
     """Tests for external API"""
 
-    @mock.patch('core.api_simulation.google.get_data', side_effect=[
-        "data_google1",
-        "data_google2",
-        "data_google3",
-    ])
-    def test_external_api_mock_setup_tear_down(self, mock_google):
+    @mock.patch('core.api_simulation.google.get_data', side_effect=Exception('Boom!'))
+    def test_external_api_mock_exception(self, mock_google):
         """Test external API calls with mock"""
-        self.assertEqual(google.call_google_api(), 'data_google1')
-        self.assertEqual(google.call_google_api(), 'data_google2')
-        self.assertEqual(google.call_google_api(), 'data_google3')
+        self.assertRaises(Exception, mock_google)
+
+    # @mock.patch('core.api_simulation.google.get_data', side_effect=[
+    #     "data_google1",
+    #     "data_google2",
+    #     "data_google3",
+    # ])
+    # def test_external_api_mock_side_effects(self, mock_google):
+    #     """Test external API calls with mock"""
+    #     self.assertEqual(google.call_google_api(), 'data_google1')
+    #     self.assertEqual(google.call_google_api(), 'data_google2')
+    #     self.assertEqual(google.call_google_api(), 'data_google3')
 
     # def setUp(self):
     #     self.patcher = mock.patch(
