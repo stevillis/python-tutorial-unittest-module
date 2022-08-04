c42a01192dfcc07cad0c2ca709ef7a891878aeae
Stévillis Sousa <stevillis@hotmail.com>
2022-08-04 15:00:52 -0400

    feat: testing with mocking and decorator

--- tests/test_fs.py
+++ tests/test_fs.py
@@ -1,6 +1,6 @@
 """Test fs module"""
 
-from unittest import TestCase
+from unittest import TestCase, mock
 
 from core import fs
 
@@ -13,4 +13,12 @@ class TestFS(TestCase):
         actual_result = fs.print_contents_of_cwd()
 
         expected_directory = b'tests'
+        self.assertIn(expected_directory, actual_result)
+
+    @mock.patch('core.fs.check_output', return_value=b'foo\nbar\n')
+    def test_print_contents_of_cwd_success_with_mocking(self, mock_check_output):
+        """Test print_contents_of_cwd function"""
+        actual_result = fs.print_contents_of_cwd()
+
+        expected_directory = b'foo'
         self.assertIn(expected_directory, actual_result)
