576d8d8bd9da8a0775823216dd35bebc8a6adf73
Stévillis Sousa <stevillis@hotmail.com>
2022-08-04 16:41:22 -0400

    feat: mocking side effects

--- tests/test_api_simulation.py
+++ tests/test_api_simulation.py
@@ -8,19 +8,30 @@ from core.api_simulation import google
 class TestAPI(TestCase):
     """Tests for external API"""
 
-    def setUp(self):
-        self.patcher = mock.patch(
-            'core.api_simulation.google.get_data',
-            return_value='data_google'
-        )
-        self.patcher.start()
-
-    def tearDown(self):
-        self.patcher.stop()
-
-    def test_external_api_mock_context(self):
+    @mock.patch('core.api_simulation.google.get_data', side_effect=[
+        "data_google1",
+        "data_google2",
+        "data_google3",
+    ])
+    def test_external_api_mock_setup_tear_down(self, mock_google):
         """Test external API calls with mock"""
-        self.assertEqual(google.call_google_api(), 'data_google')
+        self.assertEqual(google.call_google_api(), 'data_google1')
+        self.assertEqual(google.call_google_api(), 'data_google2')
+        self.assertEqual(google.call_google_api(), 'data_google3')
+
+    # def setUp(self):
+    #     self.patcher = mock.patch(
+    #         'core.api_simulation.google.get_data',
+    #         return_value='data_google'
+    #     )
+    #     self.patcher.start()
+
+    # def tearDown(self):
+    #     self.patcher.stop()
+
+    # def test_external_api_mock_setup_tear_down(self):
+    #     """Test external API calls with mock"""
+    #     self.assertEqual(google.call_google_api(), 'data_google')
 
     # def test_external_api(self):
     #     """Test external API calls"""
