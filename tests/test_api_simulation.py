1e55c26894001d3adf20741271ad96951a0d3927
Stévillis Sousa <stevillis@hotmail.com>
2022-08-04 16:38:45 -0400

    feat: mocking wit setup and teardown

--- tests/test_api_simulation.py
+++ tests/test_api_simulation.py
@@ -2,31 +2,50 @@
 
 from unittest import TestCase, mock
 
-from core.api_simulation import facebook, google
+from core.api_simulation import google
 
 
 class TestAPI(TestCase):
     """Tests for external API"""
 
-    def test_external_api(self):
-        """Test external API calls"""
-        self.assertEqual(google.call_google_api(), 'data_1')
-        self.assertEqual(facebook.call_facebook_api(), 'data_1')
+    def setUp(self):
+        self.patcher = mock.patch(
+            'core.api_simulation.google.get_data',
+            return_value='data_google'
+        )
+        self.patcher.start()
 
-    @mock.patch('core.api_simulation.google.get_data', return_value='data_google')
-    def test_external_api_mock(self, mock_google):
-        """Test external API calls with mock"""
-        self.assertEqual(google.call_google_api(), 'data_google')
-        self.assertEqual(facebook.call_facebook_api(), 'data_1')
-
-    @mock.patch('core.api_simulation.facebook.get_data', return_value='data_facebook')
-    @mock.patch('core.api_simulation.google.get_data', return_value='data_google')
-    def test_external_api_multiple_mock(self, mock_google, mock_facebook):
-        """Test external API calls with mock"""
-        self.assertEqual(google.call_google_api(), 'data_google')
-        self.assertEqual(facebook.call_facebook_api(), 'data_facebook')
+    def tearDown(self):
+        self.patcher.stop()
 
     def test_external_api_mock_context(self):
         """Test external API calls with mock"""
-        with mock.patch('core.api_simulation.google.get_data', return_value='data_google'):
-            self.assertEqual(google.call_google_api(), 'data_google')
+        self.assertEqual(google.call_google_api(), 'data_google')
+
+    # def test_external_api(self):
+    #     """Test external API calls"""
+    #     self.assertEqual(google.call_google_api(), 'data_1')
+    #     self.assertEqual(facebook.call_facebook_api(), 'data_1')
+
+    # def test_external_api(self):
+    #     """Test external API calls"""
+    #     self.assertEqual(google.call_google_api(), 'data_1')
+    #     self.assertEqual(facebook.call_facebook_api(), 'data_1')
+
+    # @mock.patch('core.api_simulation.google.get_data', return_value='data_google')
+    # def test_external_api_mock(self, mock_google):
+    #     """Test external API calls with mock"""
+    #     self.assertEqual(google.call_google_api(), 'data_google')
+    #     self.assertEqual(facebook.call_facebook_api(), 'data_1')
+
+    # @mock.patch('core.api_simulation.facebook.get_data', return_value='data_facebook')
+    # @mock.patch('core.api_simulation.google.get_data', return_value='data_google')
+    # def test_external_api_multiple_mock(self, mock_google, mock_facebook):
+    #     """Test external API calls with mock"""
+    #     self.assertEqual(google.call_google_api(), 'data_google')
+    #     self.assertEqual(facebook.call_facebook_api(), 'data_facebook')
+
+    # def test_external_api_mock_context(self):
+    #     """Test external API calls with mock"""
+    #     with mock.patch('core.api_simulation.google.get_data', return_value='data_google'):
+    #         self.assertEqual(google.call_google_api(), 'data_google')
