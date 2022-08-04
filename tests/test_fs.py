6852020d3498ffb3b948a789089562b9eefa10ff
Stévillis Sousa <stevillis@hotmail.com>
2022-08-04 15:08:24 -0400

    feat: testing with mocking inline

--- tests/test_fs.py
+++ tests/test_fs.py
@@ -8,25 +8,40 @@ from core import fs
 class TestFS(TestCase):
     """Tests fs functions"""
 
-    def test_print_contents_of_cwd_success(self):
-        """Test print_contents_of_cwd function"""
-        actual_result = fs.print_contents_of_cwd()
+    def setUp(self) -> None:
+        self.patcher = mock.patch(
+            'core.fs.check_output', return_value=b'foo\nbar\n')
+        self.patcher.start()
 
-        expected_directory = b'tests'
-        self.assertIn(expected_directory, actual_result)
+    # def test_print_contents_of_cwd_success(self):
+    #     """Test print_contents_of_cwd function"""
+    #     actual_result = fs.print_contents_of_cwd()
 
-    @mock.patch('core.fs.check_output', return_value=b'foo\nbar\n')
-    def test_print_contents_of_cwd_success_with_mocking(self, mock_check_output):
+    #     expected_directory = b'tests'
+    #     self.assertIn(expected_directory, actual_result)
+
+    # @mock.patch('core.fs.check_output', return_value=b'foo\nbar\n')
+    # def test_print_contents_of_cwd_success_with_mocking(self, mock_check_output):
+    #     """Test print_contents_of_cwd function"""
+    #     actual_result = fs.print_contents_of_cwd()
+
+    #     expected_directory = b'foo'
+    #     self.assertIn(expected_directory, actual_result)
+
+    # def test_print_contents_of_cwd_success_with_mocking_context(self):
+    #     """Test print_contents_of_cwd function"""
+    #     with mock.patch('core.fs.check_output', return_value=b'foo\nbar\n'):
+    #         actual_result = fs.print_contents_of_cwd()
+
+    #     expected_directory = b'foo'
+    #     self.assertIn(expected_directory, actual_result)
+
+    def test_print_contents_of_cwd_success_with_mocking_inline(self):
         """Test print_contents_of_cwd function"""
         actual_result = fs.print_contents_of_cwd()
 
         expected_directory = b'foo'
         self.assertIn(expected_directory, actual_result)
 
-    def test_print_contents_of_cwd_success_with_mocking_context(self):
-        """Test print_contents_of_cwd function"""
-        with mock.patch('core.fs.check_output', return_value=b'foo\nbar\n'):
-            actual_result = fs.print_contents_of_cwd()
-
-        expected_directory = b'foo'
-        self.assertIn(expected_directory, actual_result)
+    def tearDown(self) -> None:
+        self.patcher.stop()
