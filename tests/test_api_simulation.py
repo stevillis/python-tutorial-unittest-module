4e28d325e50755f7e4ac0ec94dbdfc25848aed5b
Stévillis Sousa <stevillis@hotmail.com>
2022-08-04 15:47:09 -0400

    feat: test with mock decorator

--- tests/test_api_simulation.py
+++ tests/test_api_simulation.py
@@ -1,6 +1,6 @@
 """Test API simulation modules"""
 
-from unittest import TestCase
+from unittest import TestCase, mock
 
 from core.api_simulation import facebook, google
 
@@ -11,4 +11,10 @@ class TestAPI(TestCase):
     def test_external_api(self):
         """Test external API calls"""
         self.assertEqual(google.call_google_api(), 'data_1')
+        self.assertEqual(facebook.call_facebook_api(), 'data_1')
+
+    @mock.patch('core.api_simulation.google.get_data', return_value='data_google')
+    def test_external_api_mock(self, mock_google):
+        """Test external API calls with mock"""
+        self.assertEqual(google.call_google_api(), 'data_google')
         self.assertEqual(facebook.call_facebook_api(), 'data_1')
