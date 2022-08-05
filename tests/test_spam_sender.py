38486a6ca3d7b6af089fa4d6fc1e8a01be21d21a
Stévillis Sousa <stevillis@hotmail.com>
2022-08-04 23:38:16 -0400

    feat: test email channel call

--- tests/test_spam_sender.py
+++ tests/test_spam_sender.py
@@ -9,8 +9,34 @@ from core.dev_pro.spam_sender import SpamSender
 class EmailChannelMock:  # pylint: disable=too-few-public-methods
     """Fake Email channel class"""
 
+    def __init__(self) -> None:
+        self._send_method_called = False
+        self._send_args = []
+
+    @property
+    def send_method_called(self):
+        """Getter for send_method_called"""
+        return self._send_method_called
+
+    @send_method_called.setter
+    def send_method_called(self, value):
+        """Setter for send_method_called"""
+        self._send_method_called = value
+
+    @property
+    def send_args(self):
+        """Getter for send_args"""
+        return self._send_args
+
+    @send_args.setter
+    def send_args(self, value):
+        """Setter for send_args"""
+        self._send_args = value
+
     def send(self, receiver: str, msg: str) -> Tuple[str, str]:  # pylint: disable=no-self-use, unused-argument
         """Simulate message sending to receiver"""
+        self.send_method_called = True
+        self.send_args = [receiver, msg]
         return 'john.cena@gmail.com', 'John Cena, Lorem ipsum dolor sit amet'
 
 
@@ -51,6 +77,21 @@ class SpamSenderTestCase(unittest.TestCase):
             list(spam_sender.send_spam('Lorem ipsum dolor sit amet')),
             expected_result
         )
+
+    def test_email_channel_called(self):
+        """Test if mocked email channel was called"""
+        spam_sender = SpamSender([self.receivers[0]])
+        email_channel_mock = EmailChannelMock()
+        spam_sender.email_channels = [
+            email_channel_mock
+        ]  # dependency injection
+
+        message = 'Lorem ipsum dolor sit amet'
+        list(spam_sender.send_spam(message))
+        self.assertTrue(email_channel_mock.send_method_called)
+        self.assertEqual(email_channel_mock.send_args,
+                         [self.receivers[0], message]
+                         )
 
 
 if __name__ == '__main__':
